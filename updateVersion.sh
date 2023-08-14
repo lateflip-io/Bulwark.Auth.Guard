a62a9660ed4dd2869e7051c75830d87fb28a0691
Fritz (Fredrick Seitz) <fredrick.seitz@gmail.com>
2023-08-14 14:19:17 -0400

    chore: use github env instead of out-put

--- updateVersion.sh
+++ updateVersion.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-echo "::set-output name=new_release_version::$1"
+echo "{new_release_version}={$1}" >> $GITHUB_OUTPUT
 sed -i "s#<PackageVersion>.*#<PackageVersion>$1</PackageVersion>#" $2
 sed -i "s#<Version>.*#<Version>$1</Version>#" $2
